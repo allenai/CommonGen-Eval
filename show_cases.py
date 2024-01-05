17907692ee43c5b7599bbb463bee7470dbbeff62
yuchenlin <yuchenl@allenai.org>
2024-01-05 00:54:15 +0000

    update case studies

--- show_cases.py
+++ show_cases.py
@@ -6,10 +6,22 @@ with open("eval_outputs/aggregated_eval_results.json", "r") as f:
 
 
 def visualize_case(item):
-    print(f"- Concepts: {item['concept_set']}")
-    print(f"- Human Reference: {item['human_ref']}")
+    print("""<details>
+    <summary> Example 1 </summary>\n""")
+    print(f"- **Concepts**: `{item['concept_set']}`")
+    print(f"- **Human Reference**: `{item['human_ref']}`")
     for model_name, o in item["model_outputs"].items():
-        print(f"- {model_name}: {o['output']}")
-        print(f"\t\t ----> vs Human = {o['result']}")
-    
+        cover_all = len(o["found_words"]) == len(item["concept_set"])
+        pos_all = len(o["found_pos_words"]) == len(item["concept_set"])
+        print(f"- **{model_name}**: `{o['output']}`")
+        print(f"--> ```Versus={o['result']}; Cover={cover_all}; POS={pos_all}; Len={o['len']}```")
+    print("\n</details>\n\n")    
+
 visualize_case(data["f8efb2bbd593ee6e943cfa79aa347e2e#1"])
+
+visualize_case(data["de5f30479745102be44f5f1f572730bc#0"])
+
+visualize_case(data["81ec84a05e29c1b0066ebbc778ef9ae5#0"])
+
+visualize_case(data["7b78772b4f86d8834751a0d852c0152d#0"])
+
